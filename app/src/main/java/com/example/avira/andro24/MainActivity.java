8cb5370b329547e45ccbe7662b5094271b1a1294
Rauhayl <rauhayl@gmail.com>
2017-07-19 11:32:07 +0500

    Initial commit

---
+++ app/src/main/java/com/example/avira/andro24/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.avira.andro24;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
